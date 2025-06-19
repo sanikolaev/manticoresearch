08acba24e4a3ff7b6d98a7d7d5b089bbd4bc2b7d
Sergey Nikolaev <prostuda@academ.org>
2025-06-19 22:50:25 +0700

    Update GetRE2.cmake

--- cmake/GetRE2.cmake
+++ cmake/GetRE2.cmake
@@ -8,40 +8,45 @@
 # implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 # See the License for more information.
 #=============================================================================
-# This file need to get RE2 library sources
-# First it try 'traditional' way - find RE2 lib.
-# Then (if it is not found) it try to look into ${LIBS_BUNDLE} for file named 're2-master.zip'
-# It is supposed, that file (if any) contains github's master archive of RE2 sources.
-# If no file found, it will  try to fetch it from github, address
-# https://github.com/manticoresoftware/re2/archive/master.zip
+# This file retrieves RE2 library sources and applies patches if needed.
+# It first tries to find RE2 installed on the system.
+# If not found, it looks in ${LIBS_BUNDLE} for re2-${RE2_BRANCH}.zip,
+# and finally tries to download it from GitHub.
 
-set ( RE2_REPO "https://github.com/manticoresoftware/re2" )
-set ( RE2_BRANCH "2015-06-01" ) # specific tag for reproducable builds
-set ( RE2_SRC_MD5 "023053ef20051a0fc5911a76869be59f" )
+set(RE2_REPO "https://github.com/manticoresoftware/re2")
+set(RE2_BRANCH "2015-06-01") # specific tag for reproducible builds
+set(RE2_SRC_MD5 "023053ef20051a0fc5911a76869be59f")
 
-set ( RE2_GITHUB "${RE2_REPO}/archive/${RE2_BRANCH}.zip" )
-set ( RE2_BUNDLE "${LIBS_BUNDLE}/re2-${RE2_BRANCH}.zip" )
+set(RE2_GITHUB "${RE2_REPO}/archive/${RE2_BRANCH}.zip")
+set(RE2_BUNDLE "${LIBS_BUNDLE}/re2-${RE2_BRANCH}.zip")
 
-cmake_minimum_required ( VERSION 3.17 FATAL_ERROR )
-include ( update_bundle )
+cmake_minimum_required(VERSION 3.17 FATAL_ERROR)
+include(update_bundle)
 
-# if it is allowed to use system library - try to use it
-if (NOT WITH_RE2_FORCE_STATIC)
-	find_package ( re2 MODULE QUIET )
-	return_if_target_found ( re2::re2 "as default (sys or other) lib" )
-endif ()
+# If allowed to use system library, try to use it
+if(NOT WITH_RE2_FORCE_STATIC)
+	find_package(re2 MODULE QUIET)
+	return_if_target_found(re2::re2 "as default (sys or other) lib")
+endif()
 
-# determine destination folder where we expect pre-built re2
-find_package ( re2 QUIET CONFIG )
-return_if_target_found ( re2::re2 "found ready (no need to build)" )
+# Try to find a prebuilt RE2
+find_package(re2 QUIET CONFIG)
+return_if_target_found(re2::re2 "found ready (no need to build)")
 
-function ( PATCH_GIT RESULT RE2_SRC )
-	find_package ( Git QUIET )
-	if (NOT GIT_EXECUTABLE)
+function(PATCH_GIT RESULT RE2_SRC)
+	find_package(Git QUIET)
+	if(NOT GIT_EXECUTABLE)
 		message(STATUS "Git not found. Skipping patch_git.")
 		set(${RESULT} FALSE PARENT_SCOPE)
-		return ()
-	endif ()
+		return()
+	endif()
+
+	set(PATCH_FILE "${RE2_SRC}/libre2.patch")
+	message(STATUS "Trying to apply Git patch...")
+	message(STATUS "Patch file: ${PATCH_FILE}")
+	
+	file(READ "${PATCH_FILE}" PATCH_CONTENT)
+	message(STATUS "Patch content:\n${PATCH_CONTENT}")
 
 	execute_process(
 		COMMAND "${GIT_EXECUTABLE}" apply libre2.patch
@@ -51,24 +56,31 @@ function ( PATCH_GIT RESULT RE2_SRC )
 		ERROR_VARIABLE PATCH_STDERR
 	)
 
-	if (PATCH_RESULT EQUAL 0)
+	if(PATCH_RESULT EQUAL 0)
 		message(STATUS "Patch applied successfully with Git.")
 		set(${RESULT} TRUE PARENT_SCOPE)
-	else ()
+	else()
 		message(WARNING "Git patch failed with error code: ${PATCH_RESULT}")
 		message(WARNING "Git patch stderr: ${PATCH_STDERR}")
 		message(WARNING "Git patch stdout: ${PATCH_STDOUT}")
 		set(${RESULT} FALSE PARENT_SCOPE)
-	endif ()
-endfunction ()
+	endif()
+endfunction()
 
-function ( PATCH_PATCH RESULT RE2_SRC )
-	find_program ( PATCH_PROG patch )
-	if (NOT PATCH_PROG)
+function(PATCH_PATCH RESULT RE2_SRC)
+	find_program(PATCH_PROG patch)
+	if(NOT PATCH_PROG)
 		message(STATUS "System patch tool not found. Skipping patch_patch.")
 		set(${RESULT} FALSE PARENT_SCOPE)
-		return ()
-	endif ()
+		return()
+	endif()
+
+	set(PATCH_FILE "${RE2_SRC}/libre2.patch")
+	message(STATUS "Trying to apply patch with system 'patch' tool...")
+	message(STATUS "Patch file: ${PATCH_FILE}")
+
+	file(READ "${PATCH_FILE}" PATCH_CONTENT)
+	message(STATUS "Patch content:\n${PATCH_CONTENT}")
 
 	execute_process(
 		COMMAND "${PATCH_PROG}" --verbose -p1 --binary -i libre2.patch
@@ -78,50 +90,50 @@ function ( PATCH_PATCH RESULT RE2_SRC )
 		ERROR_VARIABLE PATCH_STDERR
 	)
 
-	if (PATCH_RESULT EQUAL 0)
+	if(PATCH_RESULT EQUAL 0)
 		message(STATUS "Patch applied successfully with system patch.")
 		set(${RESULT} TRUE PARENT_SCOPE)
-	else ()
+	else()
 		message(WARNING "System patch failed with error code: ${PATCH_RESULT}")
 		message(WARNING "System patch stderr: ${PATCH_STDERR}")
 		message(WARNING "System patch stdout: ${PATCH_STDOUT}")
 		message(WARNING "Tip: This may happen if RE2 version does not match the patch.")
 		set(${RESULT} FALSE PARENT_SCOPE)
-	endif ()
-endfunction ()
+	endif()
+endfunction()
 
-# cb to finalize RE2 sources (patch, add cmake)
-function ( PREPARE_RE2 RE2_SRC )
-	if (EXISTS "${RE2_SRC}/is_patched.txt")
+# Finalize RE2 sources (apply patch, add CMake config)
+function(PREPARE_RE2 RE2_SRC)
+	if(EXISTS "${RE2_SRC}/is_patched.txt")
 		message(STATUS "RE2 already patched. Skipping.")
-		return ()
-	endif ()
+		return()
+	endif()
 
 	message(STATUS "Copying patch to RE2 source dir...")
-	file ( COPY "${MANTICORE_SOURCE_DIR}/libre2/libre2.patch" DESTINATION "${RE2_SRC}" )
+	file(COPY "${MANTICORE_SOURCE_DIR}/libre2/libre2.patch" DESTINATION "${RE2_SRC}")
 
 	patch_git(PATCHED ${RE2_SRC})
-	if (NOT PATCHED)
+	if(NOT PATCHED)
 		patch_patch(PATCHED ${RE2_SRC})
-	endif ()
+	endif()
 
-	if (NOT PATCHED)
+	if(NOT PATCHED)
 		message(FATAL_ERROR "Couldn't patch RE2 distro. Patch failed with both Git and system 'patch'. Please verify that the patch matches the RE2 version (${RE2_BRANCH}) and that the source files are unmodified.")
-	endif ()
+	endif()
 
-	file ( WRITE "${RE2_SRC}/is_patched.txt" "ok" )
-	execute_process ( COMMAND ${CMAKE_COMMAND} -E copy_if_different "${MANTICORE_SOURCE_DIR}/libre2/CMakeLists.txt" "${RE2_SRC}/CMakeLists.txt" )
-endfunction ()
+	file(WRITE "${RE2_SRC}/is_patched.txt" "ok")
+	execute_process(COMMAND ${CMAKE_COMMAND} -E copy_if_different "${MANTICORE_SOURCE_DIR}/libre2/CMakeLists.txt" "${RE2_SRC}/CMakeLists.txt")
+endfunction()
 
-# prepare sources
-select_nearest_url ( RE2_PLACE re2 ${RE2_BUNDLE} ${RE2_GITHUB} )
-fetch_and_check ( re2 ${RE2_PLACE} ${RE2_SRC_MD5} RE2_SRC )
-prepare_re2 ( ${RE2_SRC} )
+# Prepare sources
+select_nearest_url(RE2_PLACE re2 ${RE2_BUNDLE} ${RE2_GITHUB})
+fetch_and_check(re2 ${RE2_PLACE} ${RE2_SRC_MD5} RE2_SRC)
+prepare_re2(${RE2_SRC})
 
-# build
-get_build ( RE2_BUILD re2 )
-external_build ( re2 RE2_SRC RE2_BUILD )
+# Build
+get_build(RE2_BUILD re2)
+external_build(re2 RE2_SRC RE2_BUILD)
 
-# now it should find
-find_package ( re2 REQUIRED CONFIG )
-return_if_target_found ( re2::re2 "was built" )
+# Now it should find the built library
+find_package(re2 REQUIRED CONFIG)
+return_if_target_found(re2::re2 "was built")
